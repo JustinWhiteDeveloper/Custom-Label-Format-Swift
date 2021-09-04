b863f806d1f8ec7926e7b45f840a78f2628c6d98
Justin White <justin@jwhite.co.nz>
2021-09-04 14:16:04 +1200

    * fix unit test that reads from bundle

--- Tests/Custom-Label-Format-SwiftTests/IdentityReaderTests.swift
+++ Tests/Custom-Label-Format-SwiftTests/IdentityReaderTests.swift
@@ -13,8 +13,24 @@ protocol IdentityReader {
 
 class FileIdentityReader: IdentityReader {
     
+    private let expectedFileType = "identity"
+    
     func readFile(source: String) -> [String : String] {
-        return [:]
+        
+        let pathExtension = (source as NSString).pathExtension
+        
+        if pathExtension != expectedFileType {
+            return [:]
+        }
+        
+        do {
+            let data = try String(contentsOfFile: source)
+            return identityMapFromString(input: data)
+        }
+        catch {
+            print(error)
+            return [:]
+        }
     }
     
     func identityMapFromString(input: String) -> [String : String] {
@@ -39,6 +55,8 @@ import XCTest
 @testable import Custom_Label_Format_Swift
 
 final class IdentityReaderTests: XCTestCase {
+    
+    let bundleResourcesFolder: String = "/contents/Resources"
     
     func testIdentityReader_MapFromString_EmptyDictionary() {
         //given
@@ -102,13 +120,15 @@ final class IdentityReaderTests: XCTestCase {
     
     func testIdentityReader_ReadFromBundle() {
         //given
-        let source = ""
+        let path = Bundle.module.path(forResource: "group1", ofType: "identity")!
         let reader: IdentityReader = FileIdentityReader()
-        
+
         //when
-        let value = reader.readFile(source: source)
+        let value = reader.readFile(source: path)
         
         //then
-        XCTAssertEqual(value, [:])
+        let expectedValue: [String: String] = ["15 Reasons - 15の理由":"-1134391404635171832",
+                                               "1999":"6383265587"]
+        XCTAssertEqual(value, expectedValue)
     }
 }
