3f5884f2b99a5c98574f00858b9c54856d3c083b
Justin White <justin@jwhite.co.nz>
2021-09-04 14:17:10 +1200

    * move identity reader class to its own file

--- Sources/Custom-Label-Format-Swift/Custom_Label_Format_Swift.swift
+++
@@ -1,3 +0,0 @@
-struct Custom_Label_Format_Swift {
-    var text = "Hello, World!"
-}
--- Tests/Custom-Label-Format-SwiftTests/IdentityReaderTests.swift
+++ Tests/Custom-Label-Format-SwiftTests/IdentityReaderTests.swift
@@ -5,52 +5,6 @@
 //  Created by Justin White on 4/09/21.
 //
 
-protocol IdentityReader {
-    func readFile(source: String) -> [String:String]
-    
-    func identityMapFromString(input: String) -> [String : String]
-}
-
-class FileIdentityReader: IdentityReader {
-    
-    private let expectedFileType = "identity"
-    
-    func readFile(source: String) -> [String : String] {
-        
-        let pathExtension = (source as NSString).pathExtension
-        
-        if pathExtension != expectedFileType {
-            return [:]
-        }
-        
-        do {
-            let data = try String(contentsOfFile: source)
-            return identityMapFromString(input: data)
-        }
-        catch {
-            print(error)
-            return [:]
-        }
-    }
-    
-    func identityMapFromString(input: String) -> [String : String] {
-        do {
-            guard let data = input.data(using: .utf8),
-                  let value = try JSONSerialization
-                        .jsonObject(with: data, options: []) as? [String: String] else {
-                return [:]
-            }
-            
-            return value
-        }
-        catch {
-            print(error)
-            return [:]
-        }
-    }
-}
-
-
 import XCTest
 @testable import Custom_Label_Format_Swift
 
